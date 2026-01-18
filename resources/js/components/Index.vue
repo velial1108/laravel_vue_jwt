7c16d667964a898890fb001736f43c8218063257
Vladislav <vlados666x7@gmail.ru>
2026-01-18 14:01:46 +0500

    feat(router.js) Setting up token verification in the router, returning to the login page if it is missing

--- resources/js/components/Index.vue
+++ resources/js/components/Index.vue
@@ -1,15 +1,34 @@
 <template>
     <div>
         <router-link :to="{name:'fruit.index'}"> List </router-link>
-        <router-link :to="{name:'user.login'}"> Login </router-link>
-        <router-link :to="{name:'user.registration'}"> Registration </router-link>
-        <router-link :to="{name:'user.personal'}"> Personal </router-link>
+        <router-link v-if="!accessToken" :to="{name:'user.login'}"> Login </router-link>
+        <router-link v-if="!accessToken" :to="{name:'user.registration'}"> Registration </router-link>
+        <router-link v-if="accessToken" :to="{name:'user.personal'}"> Personal </router-link>
         <router-view></router-view>
     </div>
 </template>
 
 <script>
     export default {
-        name: "Index"
+        name: "Index",
+        data(){
+            return{
+                accessToken: null
+            }
+        },
+        //подтягивает токен при загрузке приложения
+        mounted() {
+            this.getAccessToken()
+        },
+        //подтягивает токен при изменении и перезагрузке приложения
+        updated() {
+            this.getAccessToken()
+        },
+        methods:{
+            getAccessToken(){
+                this.accessToken = localStorage.getItem('access_token');
+            }
+        }
+
     }
 </script>
--- resources/js/router.js
+++ resources/js/router.js
@@ -1,5 +1,5 @@
 // resources/js/router.js
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 
 const routes = [
     {
@@ -22,11 +22,39 @@ const routes = [
         name: 'user.personal',
         component: () => import('./components/User/Personal.vue') // ← Заглавная I!
     },
+    //Если человек ввел что то не связанное с роутом то перекидываем его на нужную нам страницу
+    {
+        path: '/:pathMatch(.*)*',
+        name: '404',
+        component: () => import('./components/User/Personal.vue')
+    }
 ]
 
 const router = createRouter({
     history: createWebHistory(),
     routes
 })
+// beforeEach нужен чтобы выполнять действие после запуска страницы в нашем случае to показывает на какую мы пришли страницу
+// from откуда и next позволяет выполнить код текущий страницы а не остановить его
+router.beforeEach((to, from, next) => {
+    const accessToken = localStorage.getItem('access_token')
+    //получаем токен и локал storage проверяем и отправляем если что логиниться
 
+    if (!accessToken) {
+        if (to.name === 'user.login' || to.name === 'user.registration') {
+            return next()
+        } else {
+            return next({
+                name: 'user.login'
+            })
+        }
+    }
+    //если залогинен и лезет на строницу логин отправляем обратно
+    if (to.name === 'user.login' && accessToken) {
+        return next({
+            name: 'user.personal'
+        })
+    }
+    next() // Обязательно вызовите next(), иначе навигация остановится!
+})
 export default router
